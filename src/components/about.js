3f87ab41e61ff4a01d2257364de05b71eb0bddda
Francesco <francescocollotto@gmail.com>
2022-03-02 17:48:39 +0000

    add styling to about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,23 +1,24 @@
 import './about.css'
+import profilePic from '../profile-pic.jpg';
 
 const About = () => {
   return (
     <section id='about'>
-      <h1 className='text-center mb-5'>About Me</h1>
+      <h1 className='text-center fw-bold mb-5'>About Me</h1>
       <div className="container">
-        <div className="row">
-          <div className="col-12 col-sm-3">
-            <img className='img-fluid' src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'></img>
+        <div className="row gy-5">
+          <div className="col-sm-12 col-md-3">
+            <img className='img-fluid rounded' src={profilePic}></img>
           </div>
-          <div className="col-12 col-sm-9">
-          <p>
+          <div className="col-sm-12 col-md-8">
+          <p className='px-lg-5'>
               Hello World! Like most of the beginners that was the first line of code I wrote about 12 months ago, 
               I immediately fell in love with the endless possibilities of programming and since then I started my coding
               journey through self-directed learning with the basics of HTML, CSS and JS. In November '21 I decided to take
               the next step forward and I joined <a href="https://www.makers.tech/">Makers Academy</a>, a 16-week intensive coding bootcamp, where I learned to code with 
               Ruby, Rails, SQL databases, Javascript, NodeJS. I became well-versed in Object-Oriented-Programming, Test-Driven-Development 
               and working through agile methodologies with daily stand-up during group projects and weekly retrospective throughout
-              the course.
+              the course.<br />
               I am looking for a great place to bring my passion and dedication into my first role in tech. In my dream job 
               I would be surrounded by awesome developers that could guide and mentor me to improve my skills and become a great professional.
             </p>
