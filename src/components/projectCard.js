8e5c2c6435111a0a045133b5bf72f1f9016d8f93
Francesco <francescocollotto@gmail.com>
2022-03-03 16:53:04 +0000

    add custom heroku icon

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,17 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+// import herokuLogo from '../heroku-logo.svg';
+import './projectCard.css';
 
-const githubIcon = <FontAwesomeIcon icon={faGithubSquare} size="2xl"/>
+const githubIcon = <FontAwesomeIcon icon={faGithubSquare}/>
 
-const ProjectCard = ({img, title, description, repositoryURL}) => {
+const ProjectCard = ({img, title, description, repositoryURL, technologies, heroku}) => {
+  const herokuLink = heroku ? <a href={heroku} target="_blank" className="card-link link-dark"><i className='icon'></i></a> : '';
   return (
     <div className="col-12 col-md-6 col-xl-3">
-      <div className="card">
+      <div className="card h-100">
         <img src={img} className="card-img-top" alt="project" />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+          <h5 className="card-title fw-bold">{title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{technologies}</h6>
           <p className="card-text">{description}</p>
-          <a href={repositoryURL} className="card-link link-dark">{githubIcon}</a>
+        </div>
+        <div className='card-footer'>
+            <a href={repositoryURL} target="_blank" className="card-link link-dark">{githubIcon}</a>
+            {herokuLink}
         </div>
       </div>
     </div>
--- src/components/projectsList.js
+++ src/components/projectsList.js
@@ -1,29 +1,37 @@
 import ProjectCard from "./projectCard";
+import './projectList.css';
+
 const img = "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80"
+
 const projects = [
   {
     img: img,
     title: "GemJunkies",
-    description: "A social network app where you can post your thoughts and stay connected with your friends",
+    description: "A full CRUD social network app where you can post your thoughts and stay connected with your friends.",
     repositoryURL: "https://github.com/FrancescoCollotto/acebook-ruby-junkies",
+    technologies: "Ruby, Rails, Postgres, RSpec, Capybara, Bulma, Cloudinary",
+    heroku: "http://gem-junkies.herokuapp.com"
   },
   {
     img: img,
-    title: "GemJunkies",
-    description: "A social network app where you can post your thoughts and stay connected with your friends",
-    repositoryURL: "https://github.com/FrancescoCollotto/acebook-ruby-junkies",
+    title: "Devote",
+    description: "Money managing application that let you round up your everyday purchases to the nearest pound, implemented with Ruby on Rails API and React on the client side.",
+    repositoryURL: "https://github.com/FrancescoCollotto/devote_frontend",
+    technologies: "Javascript, React, Bootstrap, Ruby, Rails, Postgres",
   },
   {
     img: img,
-    title: "GemJunkies",
-    description: "A social network app where you can post your thoughts and stay connected with your friends",
-    repositoryURL: "https://github.com/FrancescoCollotto/acebook-ruby-junkies",
+    title: "MakersBnB",
+    description: "A full CRUD AirBnB clone made with Ruby where you can signup and post your place to be booked by other users.",
+    repositoryURL: "https://github.com/FrancescoCollotto/makersbnb",
+    technologies: "Ruby, Sinatra, Postgres, RSpec, Capybara",
   },
   {
     img: img,
-    title: "GemJunkies",
-    description: "A social network app where you can post your thoughts and stay connected with your friends",
-    repositoryURL: "https://github.com/FrancescoCollotto/acebook-ruby-junkies",
+    title: "Daily news",
+    description: "Single page web application to grab all the headlines from the Guardian newspaper API and display them on a page. Focus on DOM manipulation and OOP.",
+    repositoryURL: "https://github.com/FrancescoCollotto/news-summary-challenge",
+    technologies: "Javascript, Jest",
   }
 ]
 
@@ -32,7 +40,9 @@ const projectsElement = projects.map((project) => {
     img={project.img} 
     title={project.title} 
     description={project.description} 
-    repositoryURL={project.repositoryURL} 
+    repositoryURL={project.repositoryURL}
+    technologies={project.technologies} 
+    heroku={project.heroku}
   />
 })
 
@@ -42,7 +52,7 @@ const ProjectsList = () => {
     <section id="projects" className="py-5">
       <h1 className='text-center fw-bold mb-5'>Projects</h1>
       <div class="container">
-        <div class="row">
+        <div class="row gy-5">
           {projectsElement}
         </div>
       </div>
