0bf9bdb9cef134f3d2b457531aee1e43efadd7bc
Francesco <francescocollotto@gmail.com>
2022-03-04 08:52:47 +0000

    refactor projects details in separate file

---
+++ src/projectsDetails.js
@@ -0,0 +1,38 @@
+import gemJunkiesImg from './gemJunkies.png';
+import devoteImg from './devote.png';
+import makersBnbImg from './makersBnB.png';
+import guardianImg from './news.png';
+
+const projects = [
+  {
+    img: gemJunkiesImg,
+    title: "GemJunkies",
+    description: "A full CRUD social network app where you can post your thoughts and stay connected with your friends.",
+    repositoryURL: "https://github.com/FrancescoCollotto/acebook-ruby-junkies",
+    technologies: "Ruby, Rails, Postgres, RSpec, Capybara, Bulma, Cloudinary",
+    heroku: "http://gem-junkies.herokuapp.com"
+  },
+  {
+    img: devoteImg,
+    title: "Devote",
+    description: "Money managing application that let you round up your everyday purchases to the nearest pound, implemented with Ruby on Rails API and React on the client side.",
+    repositoryURL: "https://github.com/FrancescoCollotto/devote_frontend",
+    technologies: "Javascript, React, Bootstrap, Ruby, Rails, Postgres",
+  },
+  {
+    img: makersBnbImg,
+    title: "MakersBnB",
+    description: "A full CRUD AirBnB clone made with Ruby where you can signup and post your place to be booked by other users.",
+    repositoryURL: "https://github.com/FrancescoCollotto/makersbnb",
+    technologies: "Ruby, Sinatra, Postgres, RSpec, Capybara",
+  },
+  {
+    img: guardianImg,
+    title: "Daily news",
+    description: "Single page web application to grab all the headlines from the Guardian newspaper API and display them on a page. Focus on DOM manipulation and OOP.",
+    repositoryURL: "https://github.com/FrancescoCollotto/news-summary-challenge",
+    technologies: "Javascript, Jest",
+  }
+]
+
+export default projects;
