223539eb3652bf1b8644833c86e3fb37bd82fd21
Francesco <francescocollotto@gmail.com>
2022-03-07 17:01:41 +0000

    add flash messages to contact form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -9,6 +9,18 @@ const ContactForm = () => {
     subject: '',
     message: '',
   });
+  const [show, setShow] = useState({
+    success: false,
+    danger: false,
+  });
+
+  const successMessage = show.success ? (<div className="alert alert-success alert-dismissible" role="alert">
+    <strong>Email sent!</strong> I'll always try to get back to you within 24 hours.
+    <button type="button" className="btn-close" onClick={() => setShow({...show, success: false})} aria-label="Close"></button></div>) : '';
+
+  const failureMessage = show.danger ? (<div className="alert alert-danger alert-dismissible" role="alert">
+    <strong>Something went wrong..</strong> Please try again later.
+    <button type="button" className="btn-close" onClick={() => setShow({...show, danger: false})} aria-label="Close"></button></div>) : '';
 
   const handleChange = (e) => {
     setToSend({ ...toSend, [e.target.name]: e.target.value });
@@ -16,12 +28,13 @@ const ContactForm = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
-
     emailjs.sendForm('personal_contact', 'contact_form', form.current, '9ErHPkV_KcbJaCdns')
       .then((result) => {
           console.log(result.text);
+          setShow({...show, success: true})
       }, (error) => {
           console.log(error.text);
+          setShow({...show, danger: true})
       });
 
       setToSend({
@@ -33,17 +46,30 @@ const ContactForm = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" placeholder='Your name' onChange={handleChange} value={toSend.user_name} />
-      <label>Email</label>
-      <input type="email" name="user_email" placeholder='Your email' onChange={handleChange} value={toSend.user_email} />
-      <label>Subject</label>
-      <input type="text" name="subject" placeholder='Subject' onChange={handleChange} value={toSend.subject} />
-      <label>Message</label>
-      <textarea name="message" placeholder='message here...' onChange={handleChange} value={toSend.message} />
-      <input type="submit" value="Send" />
-    </form>
+    <div className='my-4'>
+      {successMessage}
+      {failureMessage}
+      <form ref={form} onSubmit={sendEmail}>
+        <div className='mb-3'>
+          <label htmlFor='name' className='form-label'>Name</label>
+          <input type="text" name="user_name" id='name' className='form-control' placeholder='Your name' onChange={handleChange} value={toSend.user_name} required/>
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='email' className='form-label'>Email</label>
+          <input type="email" name="user_email" id='email' className='form-control' placeholder='Your email' onChange={handleChange} value={toSend.user_email} required/>
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='subject' className='form-label'>Subject</label>
+          <input type="text" name="subject" id='subject' className='form-control' placeholder='Add subject' onChange={handleChange} value={toSend.subject} required/>
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='message' className='form-label'>Message</label>
+          <textarea name='message' id='message' className='form-control' placeholder='Message here...' rows="5" onChange={handleChange} value={toSend.message} required/>
+        </div>
+        
+        <input type="submit" className="btn btn-outline-light" value="Send" />
+      </form>
+    </div>
   );
 };
 
